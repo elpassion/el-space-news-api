b4cb4d27c9e202e11bcf9bf92c0979163c5af2b6
Karol Kowalski <ordon1313@gmail.com>
2017-03-07 11:34:19 +0100

    Init

---
+++ src/test/java/pl/elpassion/news/ExampleTest.kt
@@ -0,0 +1,12 @@
+package pl.elpassion.news
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class ExampleTest {
+
+    @Test
+    fun shouldPass() {
+        assertTrue(true)
+    }
+}
